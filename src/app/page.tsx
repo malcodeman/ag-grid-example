38947eff44439fe62217db93ca20524834123f80
malcodeman <26983729+malcodeman@users.noreply.github.com>
2024-01-01 23:52:48 +0100

    feat: adds mantine

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,69 @@
+"use client";
+import { Table } from "@mantine/core";
+import { useState } from "react";
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+
+type Column = {
+  title: string;
+  amount: number;
+};
+
+const columnHelper = createColumnHelper<Column>();
+const columns = [
+  columnHelper.accessor("title", {
+    header: () => "Title",
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor("amount", {
+    header: () => "Amount",
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+];
+
 export default function Home() {
-  return <main>Pecunia</main>;
+  const [data, setData] = useState([]);
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <Table>
+      <Table.Thead>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <Table.Tr key={headerGroup.id}>
+            {headerGroup.headers.map((header) => (
+              <Table.Th key={header.id}>
+                {header.isPlaceholder
+                  ? null
+                  : flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+              </Table.Th>
+            ))}
+          </Table.Tr>
+        ))}
+      </Table.Thead>
+      <Table.Tbody>
+        {table.getRowModel().rows.map((row) => (
+          <Table.Tr key={row.id}>
+            {row.getVisibleCells().map((cell) => (
+              <Table.Td key={cell.id}>
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </Table.Td>
+            ))}
+          </Table.Tr>
+        ))}
+      </Table.Tbody>
+    </Table>
+  );
 }
