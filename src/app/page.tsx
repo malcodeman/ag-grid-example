6a5d08a4ca90ca4e76a15e2070aeefbe3c87280c
malcodeman <26983729+malcodeman@users.noreply.github.com>
2024-01-06 18:24:14 +0100

    feat: adds column resize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import { Box, Container, Table } from "@mantine/core";
 import { useEffect, useState } from "react";
 import {
+  ColumnResizeMode,
   createColumnHelper,
   flexRender,
   getCoreRowModel,
@@ -39,6 +40,7 @@ const columns = [
     footer: (info) => info.column.id,
   }),
 ];
+const columnResizeMode: ColumnResizeMode = "onChange";
 
 const makeInitialData = () => {
   return Array.from({ length: 100 }, () => ({
@@ -54,6 +56,7 @@ export default function Home() {
   const table = useReactTable({
     data,
     columns,
+    columnResizeMode,
     getCoreRowModel: getCoreRowModel(),
   });
 
@@ -64,18 +67,58 @@ export default function Home() {
   return (
     <Box style={{ paddingTop: "2rem", paddingBottom: "2rem" }}>
       <Container>
-        <Table striped highlightOnHover>
+        <Table
+          striped
+          highlightOnHover
+          withColumnBorders
+          style={{ width: table.getCenterTotalSize() }}
+        >
           <Table.Thead>
             {table.getHeaderGroups().map((headerGroup) => (
               <Table.Tr key={headerGroup.id}>
                 {headerGroup.headers.map((header) => (
-                  <Table.Th key={header.id}>
+                  <Table.Th
+                    key={header.id}
+                    style={{
+                      width: header.getSize(),
+                      position: "relative",
+                    }}
+                  >
                     {header.isPlaceholder
                       ? null
                       : flexRender(
                           header.column.columnDef.header,
                           header.getContext()
                         )}
+                    <Box
+                      style={{
+                        position: "absolute",
+                        top: 0,
+                        right: 0,
+                        height: "100%",
+                        width: "5px",
+                        backgroundColor: header.column.getIsResizing()
+                          ? "white"
+                          : "rgba(0, 0, 0, 0.5)",
+                        cursor: "col-resize",
+                        userSelect: "none",
+                        touchAction: "none",
+                        transform:
+                          columnResizeMode === "onEnd" &&
+                          header.column.getIsResizing()
+                            ? `translateX(${
+                                (table.options.columnResizeDirection === "rtl"
+                                  ? -1
+                                  : 1) *
+                                (table.getState().columnSizingInfo
+                                  .deltaOffset ?? 0)
+                              }px)`
+                            : "",
+                      }}
+                      onDoubleClick={() => header.column.resetSize()}
+                      onMouseDown={header.getResizeHandler()}
+                      onTouchStart={header.getResizeHandler()}
+                    />
                   </Table.Th>
                 ))}
               </Table.Tr>
@@ -85,7 +128,12 @@ export default function Home() {
             {table.getRowModel().rows.map((row) => (
               <Table.Tr key={row.id}>
                 {row.getVisibleCells().map((cell) => (
-                  <Table.Td key={cell.id}>
+                  <Table.Td
+                    key={cell.id}
+                    style={{
+                      width: cell.column.getSize(),
+                    }}
+                  >
                     {flexRender(cell.column.columnDef.cell, cell.getContext())}
                   </Table.Td>
                 ))}
